sreeks/Selenium2

commit 532dd2068067b032d7f9bffcf108183e25a807fd

SimonStewart: A fairly large change to how we instantiate browser instances for the java tests. I suspect that this is going to introduce some test instability. The next logical change is to allow java_test targets to specify browsers in the same way as js and python tests.
git-svn-id: https://selenium.googlecode.com/svn/trunk@15211 07704840-8298-11de-bf8c-fd130f914ac9

java/client/test/org/openqa/selenium/javascript/JsTestSuiteBuilder.java
@@ -29,10 +29,10 @@ import com.google.common.collect.Iterables;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
-import org.openqa.selenium.testing.drivers.DefaultDriverSupplierSupplier;
 import org.openqa.selenium.DriverTestDecorator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.testing.InProject;
+import org.openqa.selenium.testing.drivers.WebDriverBuilder;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -53,7 +53,7 @@ class JsTestSuiteBuilder {
   private static final boolean NO_REFRESH_DRIVER = false;
 
   private Function<String, Test> testFactory = null;
-  private Class<? extends WebDriver> driverClazz = null;
+//  private Class<? extends WebDriver> driverClazz = null;
 
   /**
    * @param driverClazz The type of {@link WebDriver} that should be used to
@@ -62,7 +62,7 @@ class JsTestSuiteBuilder {
    */
   public JsTestSuiteBuilder withDriverClazz(
       Class<? extends WebDriver> driverClazz) {
-    this.driverClazz = checkNotNull(driverClazz);
+//    this.driverClazz = checkNotNull(driverClazz);
     return this;
   }
 
@@ -82,14 +82,13 @@ class JsTestSuiteBuilder {
    */
   public Test build() {
     checkNotNull(testFactory, "No path to test function specified");
-    checkNotNull(driverClazz, "No driver class specified");
+//    checkNotNull(driverClazz, "No driver class specified");
 
     File testDirectory = getTestDirectory();
     ImmutableSet<File> excludedFiles = getExcludedFiles(testDirectory);
     String basePath = getTestUrlPath();
 
-    Supplier<WebDriver> driverSupplier =
-        new DefaultDriverSupplierSupplier(driverClazz).get();
+    Supplier<WebDriver> driverSupplier = new WebDriverBuilder();
 
     TestSuite suite = new TestSuite();
     List<File> testFiles = findTestFiles(testDirectory,

java/client/test/com/thoughtworks/selenium/InternalSelenseTestBase.java
@@ -5,7 +5,7 @@ import com.google.common.io.Files;
 import com.google.common.io.Resources;
 
 import org.openqa.selenium.Build;
-import org.openqa.selenium.DevMode;
+import org.openqa.selenium.testing.DevMode;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverBackedSelenium;

java/client/test/org/openqa/selenium/SeleniumServerInstance.java
@@ -1,6 +1,6 @@
 package org.openqa.selenium;
 
-import static org.openqa.selenium.DevMode.isInDevMode;
+import static org.openqa.selenium.testing.DevMode.isInDevMode;
 import static org.openqa.selenium.net.PortProber.pollPort;
 
 import com.google.common.collect.Maps;

java/client/test/org/openqa/selenium/TestSuiteBuilder.java
@@ -17,19 +17,25 @@ limitations under the License.
 
 package org.openqa.selenium;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertTrue;
+
 import com.google.common.base.Supplier;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-import org.openqa.selenium.testing.drivers.DefaultDriverSupplierSupplier;
 import org.openqa.selenium.testing.Ignore;
 import org.openqa.selenium.testing.IgnoreComparator;
 import org.openqa.selenium.testing.IgnoredTestCallback;
 import org.openqa.selenium.testing.InProject;
 import org.openqa.selenium.testing.drivers.SauceDriver;
+import org.openqa.selenium.testing.drivers.WebDriverBuilder;
 
 import java.io.File;
 import java.lang.reflect.AnnotatedElement;
@@ -37,11 +43,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.List;
 import java.util.Set;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.Assert.assertTrue;
 
 public class TestSuiteBuilder {
   private Set<File> sourceDirs = Sets.newHashSet();
@@ -75,6 +76,7 @@ public class TestSuiteBuilder {
 
   public TestSuiteBuilder usingDriver(Class<? extends WebDriver> ss) {
     this.driverClass = ss;
+    System.setProperty("selenium.browser.class_name", ss.getName());
     return this;
   }
 
@@ -265,7 +267,7 @@ public class TestSuiteBuilder {
           }
 
           if (withDriver && driverClass != null) {
-            Supplier<WebDriver> supplier = new DefaultDriverSupplierSupplier(driverClass).get();
+            Supplier<WebDriver> supplier = new WebDriverBuilder();
             test = new DriverTestDecorator(test, supplier,
                 keepDriver, freshDriver, restartDriver);
           }

java/client/test/org/openqa/selenium/build.desc
@@ -11,7 +11,6 @@ java_library(name = "base",
   srcs = [
     "AbstractDriverTestCase.java",
     "Build.java",
-    "DevMode.java",
     "DriverTestDecorator.java",
     "EmptyTest.java",
     "EnvironmentStarter.java",

java/client/test/org/openqa/selenium/chrome/build.desc
@@ -1,6 +1,9 @@
 
 java_test(name = "test",
   srcs = [ "*.java" ],
+  sysproperties = [
+    { "selenium.browser" : "chrome" },
+  ],
   deps = [
     "//java/client/src/org/openqa/selenium/chrome",
     "//java/client/test/org/openqa/selenium:tests",

java/client/test/org/openqa/selenium/firefox/FirefoxDriverTest.java
@@ -39,7 +39,7 @@ import com.google.common.base.Throwables;
 import org.junit.Assert;
 import org.openqa.selenium.AbstractDriverTestCase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.DevMode;
+import org.openqa.selenium.testing.DevMode;
 import org.openqa.selenium.testing.Ignore;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NeedsFreshDriver;

java/client/test/org/openqa/selenium/firefox/build.desc
@@ -26,6 +26,9 @@ java_library(name = "tests_common",
 
 java_test(name = "test_synthesized",
   srcs = [ "SynthesizedFirefoxDriverTestSuite.java" ],
+  sysproperties = [
+    { "selenium.browser" : "ff" },
+  ],
   deps = [
     ":drivers",
     ":tests_common",
@@ -33,6 +36,9 @@ java_test(name = "test_synthesized",
 
 java_test(name = "test_native",
   srcs = [ "NativeEventsFirefoxDriverTestSuite.java" ],
+  sysproperties = [
+    { "selenium.browser" : "ff" },
+  ],
   deps = [
     ":drivers",
     ":tests_common",

commit 6f1e6e42ac002449e1f553051f18739a350c35e8

JasonLeyba: Fix //javascript/webdriver:test_e2e:run, which was broken by r15211.
git-svn-id: https://selenium.googlecode.com/svn/trunk@15281 07704840-8298-11de-bf8c-fd130f914ac9

java/client/test/org/openqa/selenium/javascript/JsTestSuiteBuilder.java
@@ -53,16 +53,16 @@ class JsTestSuiteBuilder {
   private static final boolean NO_REFRESH_DRIVER = false;
 
   private Function<String, Test> testFactory = null;
-//  private Class<? extends WebDriver> driverClazz = null;
+  private Supplier<WebDriver> driverSupplier = new WebDriverBuilder();
 
   /**
-   * @param driverClazz The type of {@link WebDriver} that should be used to
-   *     run the tests.
+   * @param driverSupplier The supplier to use for acquiring {@link WebDriver}
+   *     instances when running the tests.
    * @return A self reference.
    */
-  public JsTestSuiteBuilder withDriverClazz(
-      Class<? extends WebDriver> driverClazz) {
-//    this.driverClazz = checkNotNull(driverClazz);
+  public JsTestSuiteBuilder withDriverSupplier(
+      Supplier<WebDriver> driverSupplier) {
+    this.driverSupplier = checkNotNull(driverSupplier);
     return this;
   }
 
@@ -82,13 +82,10 @@ class JsTestSuiteBuilder {
    */
   public Test build() {
     checkNotNull(testFactory, "No path to test function specified");
-//    checkNotNull(driverClazz, "No driver class specified");
 
     File testDirectory = getTestDirectory();
     ImmutableSet<File> excludedFiles = getExcludedFiles(testDirectory);
     String basePath = getTestUrlPath();
-
-    Supplier<WebDriver> driverSupplier = new WebDriverBuilder();
 
     TestSuite suite = new TestSuite();
     List<File> testFiles = findTestFiles(testDirectory,

java/client/test/org/openqa/selenium/javascript/WebDriverJsTestSuite.java
@@ -17,16 +17,6 @@ limitations under the License.
 
 package org.openqa.selenium.javascript;
 
-import com.google.common.base.Function;
-
-import junit.extensions.TestSetup;
-import junit.framework.Test;
-import junit.framework.TestCase;
-
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.openqa.selenium.DriverTestDecorator;
 import org.openqa.selenium.NeedsDriver;
 import org.openqa.selenium.Platform;
@@ -35,6 +25,17 @@ import org.openqa.selenium.environment.webserver.AppServer;
 import org.openqa.selenium.environment.webserver.Jetty7AppServer;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.google.common.base.Function;
+import com.google.common.base.Supplier;
+import com.google.common.base.Throwables;
+import junit.extensions.TestSetup;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -48,7 +49,7 @@ public class WebDriverJsTestSuite {
     final AppServer appServer = createAppServer(testEventServlet);
 
     Test test = new JsTestSuiteBuilder()
-        .withDriverClazz(RemoteWebDriverForTest.class)
+        .withDriverSupplier(createDriverSupplier())
         .withTestFactory(new Function<String, Test>() {
           public Test apply(String testPath) {
             return new WebDriverJsTestCase(testPath, appServer,
@@ -72,6 +73,18 @@ public class WebDriverJsTestSuite {
     Jetty7AppServer appServer = new Jetty7AppServer();
     appServer.addHandler(context);
     return appServer;
+  }
+  
+  private static Supplier<WebDriver> createDriverSupplier() {
+    return new Supplier<WebDriver>() {
+      public WebDriver get() {
+        try {
+          return new RemoteWebDriverForTest();
+        } catch (MalformedURLException e) {
+          throw Throwables.propagate(e);
+        }
+      }
+    };
   }
 
   private static DesiredCapabilities getCapabilities() {